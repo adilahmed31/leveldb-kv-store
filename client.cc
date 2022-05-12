bf27f114cf23322807111dc37d0f3b669b6aab1c
Basava <basava726.k@gmail.com>
2022-05-12 09:31:19 -0600

    client fetches info from zk

--- client.cc
+++ client.cc
@@ -1,37 +1,56 @@
 #include <grpcpp/grpcpp.h>
 #include <time.h>
+#include <unistd.h>
 
 #include "WifsClient.h"
 #include "wifs.grpc.pb.h"
 
+#include <conservator/ConservatorFrameworkFactory.h>
+#include <conservator/ConservatorFramework.h>
+#include <conservator/ExistsBuilder.h>
 
-std::string start_server_ip = "";
+
+std::string zk_server_ip = "127.0.0.1:2181";
+unique_ptr<ConservatorFramework> framework;
 
 static struct options {
     WifsClient* wifsclient[MAX_NUM_SERVERS];
     int show_help;
 } options;
 
-void populate_tmp_master_server_details(wifs::ServerDetails &master_details) {
-    if(start_server_ip != "") {
-        master_details.set_serverid(0);
-        master_details.set_ipaddr(start_server_ip);
-        std::cout<<start_server_ip<<"\n";
-        return;
-    }
+void init_zk_connection() {
+    ConservatorFrameworkFactory factory = ConservatorFrameworkFactory();
+    framework = factory.newClient(zk_server_ip.c_str(),1000);
+    framework->start();
+}
 
-    master_details.set_serverid(0);
-    char arr[500];
-    gethostname(arr, 500);
-    std::cout<<arr<<"\n";
-    master_details.set_ipaddr(arr);
+void populate_tmp_master_server_details(wifs::ServerDetails &master_details) {
+    if(framework->checkExists()->forPath("/master") == ZOK) {
+        std::cout<<"ZOK\n";
+        std::string master_server_details_str = framework->getData()->forPath("/master");
+        std::cout<<master_server_details_str<<"\n";
+        int delim_pos = (int) master_server_details_str.find(":");
+        master_details.set_ipaddr(master_server_details_str.substr(0, delim_pos));
+        std::cout<<master_details.ipaddr()<<"\n";
+        master_details.set_serverid(atoi(master_server_details_str.substr(delim_pos+1, 5).c_str()) - 50060);
+        std::cout<<master_details.serverid()<<"\n";
+        return;
+    } 
+    std::cout<<"zk doesn't have master ip, something is wrong. exit\n";
+    std::exit(0);
 }
 
 void init_tmp_master(){
     // needs to know master 
     wifs::ServerDetails master_details;
     populate_tmp_master_server_details(master_details);
+    std::cout<<getP2PServerAddr(master_details)<<"\n";
     server_map[somehashfunction(getP2PServerAddr(master_details))] = master_details;
+}
+
+void reset_client_cache() {
+    server_map.clear();
+    framework->close();
 }
 
 extern "C" {
@@ -41,29 +60,40 @@ extern "C" {
 
     int do_get(char* key, char* val) { //mode 0 for default, 1 for batch reads
         if (server_map.empty()){
+            init_zk_connection();
             init_tmp_master();
         }
         auto it = server_map.lower_bound(somehashfunction(std::string(key)));
         if(it == server_map.end()) it = server_map.begin();
         if(options.wifsclient[it->second.serverid()] == NULL) init(it->second);
         int rc = options.wifsclient[it->second.serverid()]->wifs_GET(key, val);
+        if(rc < 0) {
+            reset_client_cache();
+            return do_get(key, val);
+        }
         return rc;
     }
 
     int do_getRange(char* prefix, std::vector<wifs::KVPair>* batch_read) { 
         if (server_map.empty()){
+            init_zk_connection();
             init_tmp_master();
         }
         auto it = server_map.lower_bound(somehashfunction(std::string(prefix)));
         if(it == server_map.end()) it = server_map.begin();
         if(options.wifsclient[it->second.serverid()] == NULL) init(it->second);
         int rc = options.wifsclient[it->second.serverid()]->wifs_GETRANGE(prefix, *batch_read);
+        if(rc < 0) {
+            reset_client_cache();
+            return do_getRange(prefix, batch_read);
+        }
         return rc;
     }
 
     int do_getRange_nilext(char* prefix){ 
         //just for performance testing
         if (server_map.empty()){
+            init_zk_connection();
             init_tmp_master();
         }
         std::vector<wifs::KVPair> batch_read;
@@ -72,28 +102,42 @@ extern "C" {
         if(options.wifsclient[it->second.serverid()] == NULL) init(it->second);
         int rc = options.wifsclient[it->second.serverid()]->wifs_GETRANGE(prefix, batch_read);
         if(rc==0) return batch_read.size();
+        if(rc < 0) {
+            reset_client_cache();
+            return do_getRange_nilext(prefix);
+        }
         return rc;
     }
 
     int do_put(char* key, char* val) {
         if (server_map.empty()){
+            init_zk_connection();
             init_tmp_master();
         }
         auto it = server_map.lower_bound(somehashfunction(std::string(key)));
         if(it == server_map.end()) it = server_map.begin();
         if(options.wifsclient[it->second.serverid()] == NULL) init(it->second);
         int rc = options.wifsclient[it->second.serverid()]->wifs_PUT(key, val);
+        if(rc < 0) {
+            reset_client_cache();
+            return do_put(key, val);
+        }
         return rc;
     }
 
     int do_delete(char* key) {
         if (server_map.empty()){
+            init_zk_connection();
             init_tmp_master();
         }
         auto it = server_map.lower_bound(somehashfunction(std::string(key)));
         if (it == server_map.end()) it = server_map.begin();
         if (options.wifsclient[it->second.serverid()] == NULL) init(it->second);
         int rc = options.wifsclient[it->second.serverid()]->wifs_DELETE(key);
+        if(rc < 0) {
+            reset_client_cache();
+            return do_delete(key);
+        }
         return rc;
     }
 }
